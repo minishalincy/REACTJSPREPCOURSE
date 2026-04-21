57accf73e9dab41238b21a3df5b5d61bf00aac70
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-04-21 16:52:29 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = function () { //funtional expression
+  return(
+  <>
+        <Navbar/>
+  </>
+  )
+};
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = function(){
+    return(
+        <>
+        <h1>This is navbar</h1>
+        </>
+    )
+}
+
+export default Navbar
