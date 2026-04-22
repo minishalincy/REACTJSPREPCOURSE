6f51449e9d2cb9eaecd9db25979440c207f4f48b
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-04-22 23:29:40 +0530

    practice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 const App = function () { //funtional expression
   return(
   <>
         <Navbar/>
         <Hero/>
-
+        <Footer/>
   </>
   )
 };
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = function(){
+    return(
+        <>
+        <h4>This is footer Section</h4>
+        </>
+    )
+}
+
+export default Footer
