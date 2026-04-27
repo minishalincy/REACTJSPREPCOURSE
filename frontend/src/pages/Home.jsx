bfa1617039a8db680d251410c289044276e83040
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-04-27 16:50:48 +0530

    home page done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -18,13 +18,13 @@ const Home = () => {
         <div className="products">
           <img src="https://img-prd-pim.poorvika.com/product/Apple-iphone-17-pro-cosmic-orange-256gb-Front-Back-View.png" alt="iphone 17 pro max" />
           <img src="https://shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=90/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=180/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=375/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=480/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=768/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=1024/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=1280/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=1366/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=1440/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=1920/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=2560/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=3840/shop.switch.com.my/cdn/shop/files/iPad_A16_Cellular_Pink_PDP_Image_Position_1__en-US.jpg?v=1741326718&width=4000" alt="ipad" />
-          <img src="https://www.notebookcheck.net/fileadmin/Notebooks/Apple/MacBook_Pro_16_2024_M4_Pro/IMG_7593.JPG" alt="macbook pro" />
+          <img src="https://macfinder.co.uk/wp-content/uploads/2022/12/img-MacBook-Pro-Retina-14-Inch-23934.jpg" alt="macbook pro" />
         </div>
 
 
         <div className="products">
           <img src="https://static.ftshp.digital/img/p/9/9/1/6/5/1/991651.jpg" alt="iphone 17 pro max" />
-          <img src="https://tse1.mm.bing.net/th/id/OIP.CZl6AHv9kiJJI_DRvDuQrAHaHa?pid=Api&P=0&h=180" alt="ipad" />
+          <img src="https://m.media-amazon.com/images/I/71gi11m8qHL.jpg" alt="ipad" />
           <img src="https://tse2.mm.bing.net/th/id/OIP.eVjasQUSYLIpd9aVg6yteQHaKs?pid=Api&P=0&h=180" alt="macbook pro" />
         </div>
       </center>
