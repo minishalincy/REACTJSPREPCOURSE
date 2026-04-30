bf187af0161d572e7ecff464b69b571e7b53921d
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-04-30 16:22:43 +0530

    arraymethods

---
+++ arraymethods/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MapExample from './components/MapExample'
+import FIlterExample from './components/FilterExample'
+import FilterExample from './components/FilterExample'
+import ReduceExample from './components/ReduceExample'
+
+const App = () => {
+  return (
+    <>
+    <MapExample/>
+    <FilterExample/>
+    <ReduceExample/>
+    </>
+  )
+}
+
+export default App
