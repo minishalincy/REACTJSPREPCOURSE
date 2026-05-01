97868630716ea03573bb17960b737e7ed1293e59
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-05-01 20:07:30 +0530

    props

---
+++ props/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import UserData from './pages/UserData'
+
+const App = () => {
+
+  let username = "Minisha"
+  let age = 21
+  let skills = ["MongoDB", "Expressjs", "Reactjs", "Nodejs"]
+  let address = {
+    city: "bangalore",
+    state: "karnataka"
+  }
+  return (
+    <>
+    <UserData name={username} age={age} skills={skills.join(",")} address={address} />
+    </>
+  )
+}
+
+export default App
---
+++ props/src/pages/UserData.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UserData = (props) => {
+  return (
+    
+    <>
+    <h1>Name: {props.username}</h1>
+    <h1>Age: {props.age}</h1>
+    <h1>Skills: {props.skills}</h1>
+    <h1>Address: {props.address.city},{props.address.state} </h1>
+    </>
+  )
+}
+
+export default UserData
