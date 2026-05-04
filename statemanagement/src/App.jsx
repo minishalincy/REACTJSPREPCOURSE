5432a06024387dbf44fb94f994c8b88edaaa2fc6
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-05-04 09:00:22 +0530

    Learn:state management - useState

---
+++ statemanagement/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UseStateE from './components/UseStateE'
+
+const App = () => {
+  return (
+    
+    <>
+    <UseStateE/>
+    </>
+  )
+}
+
+export default App
---
+++ statemanagement/src/components/UseStateE.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const UseStateE = () => {
+
+   const[state, setState] = useState("minisha")
+
+   function change(){
+    setState("Lincy")
+   }
+  return (
+
+   
+    <>
+    <h1>{state}</h1>
+    <button onClick={change}>Click</button>
+    </>
+  )
+}
+
+export default UseStateE
