26dfbee40897a34bc68a80b046782a8be4c44e6c
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-05-07 22:45:53 +0530

    Learn: React Portal and React Toastify

--- react-portal/src/components/ToastNotify.jsx
+++ react-portal/src/components/ToastNotify.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const ToastNotify =(props)=>{
+const ToastNotify =()=>{
     
    return ReactDOM.createPortal(
     <div>
-        <h4>{props.toast}</h4>
+        {/* <h4>{props.toast}</h4> */}
        
     </div>, document.getElementById("portal")
    )
