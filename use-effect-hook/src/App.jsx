ffc93ebaa717ccb4d20151047fd709144cffd429
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-05-27 08:55:49 +0530

    Tast completed

--- use-effect-hook/src/App.jsx
+++ use-effect-hook/src/App.jsx
@@ -1,39 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-const App = ()=>{
-  
-  const [users, setUsers] = useState([])
-  useEffect(()=>{
+const App = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
     fetch("https://api.github.com/users")
-    .then(x=> x.json())
-    .then(y => setUsers(y))
-  },[]) //[] dependencies to execute
+      .then((x) => x.json())
+      .then((y) => setUsers(y));
+  }, []); //[] dependencies to execute
 
-  return(
+  return (
     <>
+      <a href="" style={{color:"white"}}><center className="user">Users</center></a>
+      <div className="container">
+        {users.map((m) => {
+          return (
+            <div className="card" key={m.id}>
+              <h3>ID: {m.id}</h3>
 
-    <div className="container">
-      {/* <center>Users</center> */}
-  {
-    users.map((m) => {
-      return (
-        <div className="card" key={m.id}>
-          <h3>ID: {m.id}</h3>
+              <img src={m.avatar_url} alt="" />
 
-          <img src={m.avatar_url} alt="" />
+              <h2>Username: {m.login}</h2>
 
-          <h2>Username: {m.login}</h2>
+              <h4>Type: {m.type}</h4>
 
-          <h4>Type: {m.type}</h4>
-
-          <button>Add</button>
-        </div>
-      )
-    })
-  }
-</div>
+              <button>Add</button>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
